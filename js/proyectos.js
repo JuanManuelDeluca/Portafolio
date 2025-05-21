ef7fdf53bf73abd1f08a25fae388d293b8341249
JuanManuelDeluca <jmgdeluca@gmail.com>
2025-05-20 23:03:20 -0300

    Update proyectos.js

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,25 +1,18 @@
 const contenedor = document.getElementById("lista-proyectos");
 
-fetch("./data/proyectos.json")
-  .then(response => response.json())
-  .then(proyectos => {
-    proyectos.forEach(p => {
-      const div = document.createElement("div");
-      div.className = "proyecto";
+proyectos.forEach(p => {
+  const div = document.createElement("div");
+  div.className = "proyecto";
 
-      // Hacer clic en todo el div redirige al link
-      div.onclick = () => {
-        window.open(p.link1, "_blank");
-      };
+  // Hacer clic en todo el div redirige al link
+  div.onclick = () => {
+    window.open(p.link1, "_blank");
+  };
 
-      div.innerHTML = `
-        <h3>${p.titulo}</h3>
-        <p>${p.descripcion}</p>
-      `;
+  div.innerHTML = `
+    <h3>${p.titulo}</h3>
+    <p>${p.descripcion}</p>
+  `;
 
-      contenedor.appendChild(div);
-    });
-  })
-  .catch(error => {
-    console.error("Error al cargar proyectos:", error);
-  });
+  contenedor.appendChild(div);
+});
